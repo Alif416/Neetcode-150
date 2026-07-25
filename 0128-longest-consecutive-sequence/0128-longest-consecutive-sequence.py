531dfbf9e9cbfc73badb3af3372a78b924323004
Labibul Ahsan Alif <108290361+Alif416@users.noreply.github.com>
2026-07-25 08:28:52 +0600

    Time: 63 ms (19.2%), Space: 32.7 MB (92.32%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -5,7 +5,7 @@ class Solution:
         nums.sort()
         if not nums:
             return 0
-        for i in range(len(nums)):
+        for i in range(1,len(nums)):
             if nums[i-1]==nums[i]:
                 continue
             if nums[i]==nums[i-1]+1:
